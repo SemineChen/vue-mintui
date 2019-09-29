53eaca084acf0b17685a03d208f2044f2b4735b3
chenyueping <2311462183@qq.com>
2019-09-29 18:12:43 +0800

    190929提交

--- src/components/HomeContainer.vue
+++ src/components/HomeContainer.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <!----轮播图----->
-<mt-swipe :auto="4000">
+<mt-swipe :auto="4000" class="homeswiper">
   <mt-swipe-item>1</mt-swipe-item>
   <mt-swipe-item>2</mt-swipe-item>
   <mt-swipe-item>3</mt-swipe-item>
@@ -13,11 +13,35 @@
 </template>
 
 <script>
+export default{
+  data(){
+    return{
+
+    }
+  },
+  created(){
+    this.getlunbo()
+  },
+  methods:{
+    getlunbo(){
+         this.$http.get('https://source.unsplash.com/random').then(
+           function(result){
+              console.log(result.body)
+           }
+         
+           )
+    }
+ 
+  }
+}
 
 
 </script>
 
 <style lang="scss" scoped>
-
+.homeswiper{
+  height: 400px;
+  background: red;
+}
 
 </style>
