a28bdedf5aad914de538de4efab8276486651e42
chenyueping <2311462183@qq.com>
2019-10-08 19:19:33 +0800

    'last'

--- src/components/HomeContainer.vue
+++ src/components/HomeContainer.vue
@@ -2,10 +2,25 @@
 <div>
   <!----轮播图----->
 <mt-swipe :auto="4000" class="homeswiper">
-  <mt-swipe-item>1</mt-swipe-item>
-  <mt-swipe-item>2</mt-swipe-item>
-  <mt-swipe-item>3</mt-swipe-item>
+  <mt-swipe-item><img src="../assets/images/img1.jpg"></mt-swipe-item>
+  <mt-swipe-item><img src="../assets/images/img2.jpg"></mt-swipe-item>
+  <mt-swipe-item><img src="../assets/images/img3.jpg"></mt-swipe-item>
 </mt-swipe>
+<!-- 四宫格 -->
+ <ul class="mui-table-view mui-grid-view mui-grid-9">
+		<li class="mui-table-view-cell mui-media mui-col-xs-6 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-home"></span>
+		                    <div class="mui-media-body">Home</div></a></li>
+		<li class="mui-table-view-cell mui-media mui-col-xs-6 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+		                    <div class="mui-media-body">Email</div></a></li>
+		<li class="mui-table-view-cell mui-media mui-col-xs-6 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		<li class="mui-table-view-cell mui-media mui-col-xs-6 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		        </ul> 
 
 </div>
 
@@ -14,25 +29,25 @@
 
 <script>
 export default{
-  data(){
-    return{
+  // data(){
+  //   return{
 
-    }
-  },
-  created(){
-    this.getlunbo()
-  },
-  methods:{
-    getlunbo(){
-         this.$http.get('https://source.unsplash.com/random').then(
-           function(result){
-              console.log(result.body)
-           }
+  //   }
+  // },
+  // created(){
+  //   this.getlunbo()
+  // },
+  // methods:{
+  //   getlunbo(){
+  //        this.$http.get('https://source.unsplash.com/random').then(
+  //          function(result){
+  //             console.log(result.body)
+  //          }
          
-           )
-    }
+  //          )
+  //   }
  
-  }
+  // }
 }
 
 
@@ -40,7 +55,7 @@ export default{
 
 <style lang="scss" scoped>
 .homeswiper{
-  height: 400px;
+  height: 200px;
   background: red;
 }
 
